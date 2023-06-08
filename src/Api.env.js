b9c2ea84d089394d11e179a6bedafa340481edc7
silly <>
2023-06-08 19:43:22 +0900

    no message

---
+++ src/Api.env.js
@@ -0,0 +1,13 @@
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBynMoFpXLNaj43wh2fPDsX-Agb8KnDwyY",
+    authDomain: "chat-app2-7a67e.firebaseapp.com",
+    projectId: "chat-app2-7a67e",
+    storageBucket: "chat-app2-7a67e.appspot.com",
+    messagingSenderId: "516331415404",
+    appId: "1:516331415404:web:0b73a7dfa848fc5deef549",
+};
+
+export default firebaseConfig
